15a66deebef5471487767589f6002fbf54d2d64e
Chad Logsdon <cavejohnson93@gmail.com>
2021-08-06 19:34:47 +0000

    Fix Title Helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
@@ -14,7 +14,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}" 
+    assert_select "title", "#{@base_title}" 
     end
 
   test "should get help" do
